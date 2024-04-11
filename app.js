eb580885feaca3de4bc5f2630f41ff04f838a650
nakshjain <nakshatrajain246@gmail.com>
2024-04-11 16:52:14 +0530

    Added sitemap

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express =require('express');
 const dotenv=require("dotenv")
 const app=express();
 const cors = require('cors');
+const xmlbuilder = require('xmlbuilder');
+
+const PORT=process.env.PORT;
+
 dotenv.config({path: './config.env'})
 require('./db/mongoDb')
 
@@ -13,7 +17,28 @@ app.use(cors({
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.json())
-const PORT=process.env.PORT;
+
+const routes = [
+    '',
+    'home',
+    'shop',
+    // Add more routes as needed
+];
+
+app.get('/sitemap.xml', (req, res) => {
+    const root = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' });
+    root.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    routes.forEach(route => {
+        const url = root.ele('url');
+        url.ele('loc', `${process.env.ORIGIN_URL/route}`);
+        // You can add more elements like <changefreq> and <priority> here if needed
+    });
+
+    res.header('Content-Type', 'application/xml');
+    res.send(root.end({ pretty: true }));
+});
+
 
 const authRouter = require('./router/auth');
 app.use('/auth', authRouter);
