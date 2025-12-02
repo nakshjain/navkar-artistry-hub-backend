00a661bd19342b14a4c7edd4f6642592c9c4c395
Nakshatra Jain <nakshatrajain246@gmail.com>
2025-12-02 22:39:00 +0530

    Added tenantResolver and tenantController

---
+++ controllers/tenantController.js
@@ -0,0 +1,3 @@
+const TenantSchema = require("../model/TenantSchema");
+
+module.exports={}
---
+++ middleware/tenantResolver.js
@@ -0,0 +1,16 @@
+const Tenant = require("../model/TenantSchema");
+
+const tenantResolver = async (req, res, next) => {
+    try {
+        req.tenantId = '692f16b58234813eef6d08f6';
+        return next();
+    } catch (error) {
+        console.error("Tenant Resolver Error:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error in tenant resolver."
+        });
+    }
+};
+
+module.exports = tenantResolver;
